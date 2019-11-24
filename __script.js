21a0c9949eed7e572d33184d6572f071154b937c
Wang Da <wangda8791@gmail.com>
2019-11-25 00:48:05 +1000

    fix Content-Security-Policy for inline script execution of react

--- __script.js
+++ __script.js
@@ -1,4 +1,4 @@
-let FS= require('fs');
+let FS = require('fs');
 
 // read the index.html from build folder
 let data = FS.readFileSync('./build/index.html', 'utf8');
@@ -15,17 +15,18 @@ function insertContent(fullContent, beforeWhat, newContent) {
 }
 
 // will add the <meta> tags needed for cordova app
-const afterAddingMeta = insertContent(data, "<link", 
-`<meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *; img-src 'self' data: content:;">`+
-`<meta name="format-detection" content="telephone=no">`+
-`<meta name="msapplication-tap-highlight" content="no">`);
+const afterAddingMeta = insertContent(data, "<link",
+    `<meta http-equiv="Content-Security-Policy"
+    content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *; img-src 'self' data: content:;">`+
+    `<meta name="format-detection" content="telephone=no">` +
+    `<meta name="msapplication-tap-highlight" content="no">`);
 
 // will add <script> pointing to cordova.js
 const afterAddingScript = insertContent(afterAddingMeta, "<script", `<script type="text/javascript" src="cordova.js"></script>`);
 
 // updates the index.html file
-FS.writeFile('./build/index.html', afterAddingScript, 'utf8', (err)=> {
-    if(err) {
+FS.writeFile('./build/index.html', afterAddingScript, 'utf8', (err) => {
+    if (err) {
         throw err
     };
 })
